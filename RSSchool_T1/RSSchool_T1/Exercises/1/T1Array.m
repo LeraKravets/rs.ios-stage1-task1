4df6b08f25071a3496b0f73dce9bb3533ab7b3a9
Lera Kravets <kravetsvaleryia@gmail.com>
2020-03-16 18:25:47 +0300

    feat: did BillCounter task

--- RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
+++ RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
@@ -4,7 +4,24 @@
 
 // Complete the following fuction
 - (NSArray *)convertToHappy:(NSArray *)sadArray {
-    return @[];
+    NSMutableArray *result = [[NSMutableArray alloc] init];
+    result = [sadArray mutableCopy];
+//    for (int i = 1; i < result.count; i++) {
+//        if ([result[i] intValue] > [result[i-1] intValue] + [result[i+1] intValue]) {
+//            [result removeObjectAtIndex: i];
+//        }
+//    }
+
+
+
+//    NSMutableArray *result = [[NSMutableArray alloc] init];
+//    for (int i = 1; i < sadArray.count; i++) {
+//        while ([sadArray[i] intValue] > [sadArray[i-1] intValue] + [sadArray[i+1] intValue]) {
+//            [sadArray removeObjectAtIndex:i];
+//        }
+//    }
+    return result;
+//    [happyArray release];
 }
 
 @end
--- RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
@@ -4,7 +4,27 @@
 
 // Complete the following fuction
 - (NSString*)compareResultForBill:(NSArray<NSNumber*>*)bill notIncludingElementWithIndex:(NSInteger)index withGivenSum:(NSNumber*)sum {
-    return @"";
-}
+    NSMutableArray *resultBill = [bill mutableCopy];
+    [resultBill removeObjectAtIndex: index];
 
+    int i, resultSum, value;
+       resultSum = 0;
+       value = 0;
+
+       for (i = 0; i < [resultBill count]; i++) {
+           value = [[resultBill objectAtIndex: i] intValue];
+           resultSum += value;
+       }
+
+    resultSum = resultSum / 2;
+
+    if (resultSum != [sum intValue]) {
+        resultSum = [sum intValue] - resultSum;
+        NSLog(@"%d", resultSum);
+        NSString* myNewString = [NSString stringWithFormat:@"%i", resultSum];
+        return myNewString;
+    } else {
+        return @"Bon Appetit";
+    }
+}
 @end
