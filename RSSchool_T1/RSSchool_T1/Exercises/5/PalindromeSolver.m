fc0cd52d14a54ff0c41c934723075b867be9379e
Lera <kravetsvaleryia@gmail.com>
2020-03-17 20:54:05 +0300

    feat: did highestPalindrome task

--- RSSchool_T1/RSSchool_T1/Exercises/5/PalindromeSolver.m
+++ RSSchool_T1/RSSchool_T1/Exercises/5/PalindromeSolver.m
@@ -5,10 +5,12 @@
 // Complete the highestValuePalindrome function below.
 - (NSString *) highestValuePalindrome:(NSString *)s n:(NSNumber *)n k:(NSNumber *)k {
 
-    NSInteger lastIndex = [n intValue] - 1;
+    NSInteger highIndex = [n intValue] - 1;
     NSInteger score = 0;
-    NSInteger stopValue = 0;
+    NSInteger lowIndex = 0;
+    NSInteger stopValue = [n intValue] / 2;
     NSString *resultString = @"";
+    NSInteger changeLimit = [k intValue];
 
     NSMutableArray *charArray = [NSMutableArray array];
 
@@ -18,53 +20,65 @@
     }
     NSLog(@"%@", charArray);
 
-    if ([n intValue] % 2 == 0) {
-        stopValue = [n intValue] / 2;
-    } else {
-        stopValue = ([n intValue] - 1) / 2;
-    }
-
     for (int i = 0; i < stopValue; i++) {
         NSString *a = [charArray objectAtIndex: 0 + i];
-        NSString *b = [charArray objectAtIndex: lastIndex - i];
+        NSString *b = [charArray objectAtIndex: highIndex - i];
         NSInteger aChar = [a intValue];
         NSInteger bChar = [b intValue];
-        if (aChar != bChar && aChar > bChar) {
-            [charArray replaceObjectAtIndex: lastIndex - i withObject: a];
-            score += 1;
-        } else if (aChar != bChar && aChar < bChar) {
-            [charArray replaceObjectAtIndex: 0 + i withObject: b];
-            score += 1;
-        }
+        if (aChar != bChar) {
+                   score += 1;
+               }
     }
 
-    if (score <= [k intValue]) {
-        while (score <= ([k intValue] - 2)) {
-            [charArray replaceObjectAtIndex: lastIndex withObject: @"9"];
-            [charArray replaceObjectAtIndex: 0 withObject: @"9"];
-            score += 2;
+    if (score <= changeLimit) {
+
+        while (highIndex > lowIndex) {
+            NSString *low = [charArray objectAtIndex: lowIndex];
+            NSString *high = [charArray objectAtIndex: highIndex];
+            NSInteger lowChar = [low intValue];
+            NSInteger highChar = [high intValue];
+            if (lowChar == highChar) {
+                if (changeLimit > 1 && (changeLimit-2) >= score && lowChar != 9) {
+                    [charArray replaceObjectAtIndex: lowIndex withObject: @"9"];
+                    [charArray replaceObjectAtIndex: highIndex withObject: @"9"];
+//                    low = @"9";
+//                    high = @"9";
+                        changeLimit -= 2;
+                }
+            } else {
+                if (changeLimit > 1 && (changeLimit - 2) >= score - 1) {
+                    if (lowChar != 9){
+                        [charArray replaceObjectAtIndex: lowIndex withObject: @"9"];
+//                        low = @"9";
+                        changeLimit --;
+                    }
+                    if (highChar != 9){
+                         [charArray replaceObjectAtIndex: highIndex withObject: @"9"];
+                        //         high = @"9";
+                        changeLimit --;
+                    }
+                } else {
+                    if (lowChar > highChar){
+                        [charArray replaceObjectAtIndex: highIndex withObject: low];
+                    } else {
+                        [charArray replaceObjectAtIndex: lowIndex withObject: high];
+                    }
+                    changeLimit--;
+                }
+                score--;
+            }
+            lowIndex++;
+            highIndex--;
         }
         for (int i = 0; i < [n intValue]; i++) {
             NSString *d = [charArray objectAtIndex:i];
             resultString = [resultString stringByAppendingString:d];
+
         }
-
-//        NSString *resultString = [NSString stringWithFormat:@"%@", charArray];
-////        NSString *resultString = [NSString stringWithCString:charArray encoding:NSASCIIStringEncoding];
-////        NSString *resultString = [NSString stringWithUTF8String: charArray];
-//        NSLog(@"%@",resultString);
-
-//        NSString *resultString = [charArray componentsJoinedByString:@",\n"];
-
-
-
-//        NSString *resultString = [charArray componentsJoinedByString:@"\n "];
-//        NSString *resultString = [NSString stringWithFormat:@"%@", charArray];
         NSLog(@"%@",resultString);
         return resultString;
     } else {
         return @"-1";
     }
 }
-
 @end
