50c8e114645a22c510d1da7e1da27a80c5de0f81
Lera <kravetsvaleryia@gmail.com>
2020-03-17 21:34:20 +0300

    feat: did StringParseTests task

--- RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
+++ RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
@@ -4,7 +4,53 @@
 
 // Complete the parseString function below.
 - (NSArray <NSString*>*)parseString:(NSString*)string {
-    return @[@"Test"];
-}
 
+    NSMutableArray *stringArray = [NSMutableArray array];
+    NSMutableArray *resultArray = [NSMutableArray array];
+
+    for (int i = 0; i < [string length]; i++) {
+        char s = [string characterAtIndex:i];
+        [stringArray addObject:[NSString stringWithFormat: @"%c", s]];
+    }
+
+    for (int i = 0; i < stringArray.count; i++) {
+        if ([stringArray[i]  isEqual: @"["]) {
+            NSArray *partArray = [stringArray subarrayWithRange:NSMakeRange(i, stringArray.count - i)];
+            for (int index = 0; index < partArray.count; index++) {
+                if ([partArray[index]  isEqual: @"]"]) {
+                    if (i + 1 < index + i) {
+                        NSArray *substringArray = [stringArray subarrayWithRange:NSMakeRange(i + 1, index - 1)];
+                        [resultArray addObject: [substringArray componentsJoinedByString: @""]];
+                    }
+                }
+            }
+        } else if ([stringArray[i]  isEqual: @"("]) {
+            NSArray *partArray = [stringArray subarrayWithRange:NSMakeRange(i, stringArray.count - i)];
+            for (int index = 0; index < partArray.count; index++) {
+                if ([partArray[index]  isEqual: @")"]) {
+
+                    if (i + 1 < index + i) {
+                        NSArray *substringArray = [stringArray subarrayWithRange:NSMakeRange(i + 1, index - 1)];
+                        if ([substringArray containsObject: (@"(")]) {
+                            break;
+                        } else {
+                            [resultArray addObject: [substringArray componentsJoinedByString: @""]];
+                        }
+                    }
+                }
+            }
+        } else if ([stringArray[i]  isEqual: @"<"]) {
+            NSArray *partArray = [stringArray subarrayWithRange:NSMakeRange(i, stringArray.count - i)];
+            for (int index = 0; index < partArray.count; index++) {
+                if ([partArray[index]  isEqual: @">"]) {
+                    if (i + 1 < index + i) {
+                        NSArray *substringArray = [stringArray subarrayWithRange:NSMakeRange(i + 1, index - 1)];
+                        [resultArray addObject: [substringArray componentsJoinedByString: @""]];
+                    }
+                }
+            }
+        }
+    }
+    return resultArray;
+}
 @end
